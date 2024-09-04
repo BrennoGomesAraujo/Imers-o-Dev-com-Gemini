78d3ed348424a766174930207b4144dc601be3bc
Brenno Gomes <100851714+BrennoGomesAraujo@users.noreply.github.com>
2024-09-04 17:50:10 -0300

    Projeto Imersão dev, Alura
    Adicionando o site feito na imersão dev da Alura.

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal", 
+        decricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos [idade da Rayssa], ela já é considerada uma das maiores skatistas de sua geração",
+        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    }, 
+
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira de destaque, conhecida por sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que se destacou nas Olimpíadas de Tóquio 2020, conquistando a medalha de prata no individual geral. Sua versatilidade e determinação a tornaram um dos maiores nomes da ginástica brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    }
+    
+
+];
+
+let rayssa = {
+titulo : "Rayssa Leal", 
+decricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos [idade da Rayssa], ela já é considerada uma das maiores skatistas de sua geração",
+link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+}
