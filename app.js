227192def5df2e1b5475511ea2a3669232329cd0
Brenno Gomes <brennogomearaujo@gmail.com>
2024-09-08 10:04:26 -0300

    adicionado campoPesquisa
    Adicionado a pesquisa funcional no codigo

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 function pesquisar(){
+  
+  let section = document.getElementById('resultados-pesquisa')
+  
+
+  let campoPesquisa = document.getElementById('campo-pesquisa').value
+
+ if(!campoPesquisa){
+  section.innerHTML = "<p>Sem resultados.<br><b> Digite o nome do alteta ou esporte</p>"
+  return
+}
+
+campoPesquisa =  campoPesquisa.toLowerCase()
+
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // para cada dado dentro da lista de dados 
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase() 
+    tags = dado.tags.toLowerCase()
+
+    if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)|| tags.includes(campoPesquisa)){
+
+      resultados+= 
+      `<div class="item-resultado">
+      <h2> <a href="#" target="_blank">${dado.titulo}</a></h2>
+      <p class="descricao-meta">${dado.descricao}</p>
+      <a href= ${dado.link}" target="_self">Mais informações</a>
+      </div>`};
+    }
     
-    let section = document.getElementById('resultados-pesquisa')
-    console.log(section);
-    
-    let resultados = ""
-    
-    // para cada dado dentro da lista de dados 
-    for (let dado of dados) {resultados+= `<div class="item-resultado">
-        <h2> <a href="#" target="_blank">${dado.titulo}</a></h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href= ${dado.link}" target="_self">Mais informações</a>
-      </div>`}
-    
-    section.innerHTML = resultados
+    if (!resultados){
+      resultados = "<p>Sem resultados</p>"
+    }
+  
+  section.innerHTML = resultados
 }
 
--- dados.js
+++ dados.js
@@ -3,18 +3,21 @@ let dados = [
         titulo : "Rayssa Leal", 
         descricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos [idade da Rayssa], ela já é considerada uma das maiores skatistas de sua geração",
         link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "Skate Fada sk8"
     }, 
     
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira de destaque, conhecida por sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô brasileiro.",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "Judo ouro judoka "
     },
     
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que se destacou nas Olimpíadas de Tóquio 2020, conquistando a medalha de prata no individual geral. Sua versatilidade e determinação a tornaram um dos maiores nomes da ginástica brasileira.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "gisnastica ginástica ouro"
     }
     
 
